9c8ed3c2119bce507d69dd4d54fff47ced2776ab
huiting <331363626@qq.com>
2018-01-23 16:21:47 +0800

    分享

--- application/config.php
+++ application/config.php
@@ -140,9 +140,9 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '_CSS1_'=>'./static/css',
-        '_JS_'=>'./static/js',
-        '_IMG_'=>'./static/img',
+        '_CSS1_'=>'/static/css',
+        '_JS_'=>'/static/js',
+        '_IMG_'=>'/static/img',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
