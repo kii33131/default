f467e9a6b87a9c99791c24047ef3825a99c0f7fb
huiting <331363626@qq.com>
2018-02-04 17:14:24 +0800

    分享

--- application/index/controller/WxpayAPI/example/WxPay.JsApiPay.php
+++ application/index/controller/WxpayAPI/example/WxPay.JsApiPay.php
@@ -1,9 +1,5 @@
 <?php
-//echo __DIR__;exit;
-///var/www/html/default/application/extra/WxpayAPI/example
-$path=str_replace('example','lib',__DIR__);
-
-require_once $path."/WxPay.Api.php";
+require_once "../lib/WxPay.Api.php";
 /**
  * 
  * JSAPI支付实现类
